7a092da2eefa98e10bf1c68b3314243d280eb87a
TJ Alston <tjalston82@yahoo.com>
2016-08-24 15:05:58 -0400

    basic setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ class App extends React.Component {
 
   render () {
     return <div>
-      <h1>App App</h1>
+      <h1>MINION MEMORY</h1>
+      <main>
+        <div className='card up'>GREY</div>
+        <div className='card up'>BLUE</div>
+        <div className='card up'>PURPLE</div>
+        <div className='card up'>HOT-PINK</div>
+        <div className='card down'>BANANAS</div>
+      </main>
     </div>
   }
 }
