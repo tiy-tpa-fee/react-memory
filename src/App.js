c5c9e7dfa2ac154289e3beca68929588d5e5d78a
Darren Thomas Clark <darren.clark@live.com>
2016-08-24 15:06:48 -0400

    html/sass

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ class App extends React.Component {
 
   render () {
     return <div>
-      <h1>App App</h1>
+      <h1>Class doing memory____TOGETHHHHHHHER!!!!</h1>
+      <main>
+        <div className="card up">Jake</div>
+        <div className="card down">Fin</div>
+        <div className="card up">Princess</div>
+        <div className="card down">BMO</div>
+      </main>
     </div>
   }
 }
