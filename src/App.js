f4262e3e881d095ec7390965674a192ed30ffb2c
Chris Tsantiris <Chris.Tsantiris@gmail.com>
2016-08-26 13:07:39 -0400

    1pm edits

--- src/App.js
+++ src/App.js
@@ -8,25 +8,33 @@ class App extends React.Component {
     super()
     this.state = {
       cards: [
-        { value: 'Jake', up: false },
-        { value: 'Princess', up: false },
-        { value: 'Finn', up: false },
-        { value: 'BMO', up: false },
-        { value: 'Ice King', up: false }
-      ]
+        'Jake',
+        'Princess Bubbelgum',
+        'Finn',
+        'BMO',
+        'Ice King',
+        'Jake',
+        'Princess Bubbelgum',
+        'Finn',
+        'BMO',
+        'Ice King'
+      ],
+      matched: [5, 8],
+      turned: [2]
     }
   }
 
-  flipCard (index) {
+  flipCard = (index) => {
     const cards = this.state.cards
-    cards[index].up = !cards[index].up
+
     this.setState({ cards: cards })
   }
 
   render () {
     // let cardData = this.state.cards
     const cards = this.state.cards.map((card, index) => {
-      return <Card flipCard={this.flipCard.bind(this, index)} {...card} key={index} />
+      const up = (this.state.turned + this.state.matched).includes(index)
+      return <Card flipCard={this.flipCard} value={card} up={up} index={index} key={index} />
     })
     return <div>
       <h1>Class doing memory____TOGETHHHHHHHER!!!!</h1>
--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Card extends React.Component {
   handleClick = () => {
-    this.props.flipCard()
+    this.props.flipCard(this.props.index)
   }
 
   render () {
